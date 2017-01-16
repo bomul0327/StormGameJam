46c57085dce084dc887945ff04ab65a24e98635d
HBJ <bomul0327@naver.com>
2017-01-16 20:08:39 +0900

    네트워크 테스트용

---
+++ Assets/02. Scripts/PlayerTest.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerTest : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		if(!isLocalPlayer)
+			return;
+
+		float hor = Input.GetAxis("Horizontal");
+		float ver = Input.GetAxis("Vertical");
+
+		transform.Translate(new Vector3(hor, 0, ver) * 10f * Time.deltaTime);
+	}
+}
