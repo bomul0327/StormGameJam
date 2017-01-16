4a5d276e0ba405aef837e242b168acad2850b605
HBJ <bomul0327@naver.com>
2017-01-17 02:51:50 +0900

    2D로 변환. @상은: 톡방의 외부 에셋들 전부 ExternalAssets로 넣을 것 Unity-chan 2D캐릭터 배치
    PlayerCtrl: 기본 이동 + 점프 + 애니메이션들 재생 가능
    TODO: 캐릭터 점프 조건, 애니메이션 조건들 상세하게 설정해야함.

---
+++ Assets/02. Scripts/PlayerCtrl.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCtrl : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpSpeed = 3;
+
+	private float horInput;
+	private float verInput;
+
+	private Vector2 moveDir;
+
+	//For flipping sprite 
+	private float scale;
+
+	//Initialize Components
+	private Animator anim;
+	private Rigidbody2D rb2d;
+	
+	void Awake(){
+		scale = transform.localScale.x;
+		anim = GetComponent<Animator>();
+		rb2d = GetComponent<Rigidbody2D>();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		horInput = Input.GetAxis("Horizontal");
+		verInput = Input.GetAxis("Vertical");
+
+		moveDir = new Vector2(horInput, 0);
+		
+		FlipCharacter();
+		Attack();
+		Jump();
+		Crouch();
+
+		rb2d.velocity = moveDir * moveSpeed + new Vector2(0, rb2d.velocity.y);
+		
+		UpdateAnimation();
+	}
+
+	//Update Animation
+	void UpdateAnimation(){
+
+		anim.SetFloat("Speed", Mathf.Abs(horInput));
+		anim.SetFloat("FallSpeed", rb2d.velocity.y);
+
+	}
+
+	void FlipCharacter(){
+		if(horInput < 0){
+			transform.localScale = new Vector3(-scale, scale, 0);
+		}
+		else if(horInput > 0){
+			transform.localScale = new Vector3(scale, scale, 0);
+		}
+	}
+
+	void Jump(){
+		if(Input.GetButtonDown("Jump")){
+			rb2d.velocity = new Vector2(rb2d.velocity.x, jumpSpeed);
+		}
+	}
+
+	void Attack(){
+		if(Input.GetButtonDown("Fire1")){
+			anim.SetTrigger("Attack1");
+		}
+		if(Input.GetButtonDown("Fire2")){
+			anim.SetTrigger("Attack2");
+		}
+		if(Input.GetButtonDown("Fire3")){
+			anim.SetTrigger("Attack3");
+		}
+	}
+
+	void Crouch(){
+		if(verInput < 0){
+			anim.SetBool("IsCrouch", true);
+		}
+		else{
+			anim.SetBool("IsCrouch", false);
+		}
+	}
+
+	void GetDamage(){
+		anim.SetTrigger("Damage");
+	}
+}
