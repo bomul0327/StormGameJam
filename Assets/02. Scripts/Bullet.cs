afcd9519cb0be62de765221c0aea2bd84fae4741
HBJ <bomul0327@naver.com>
2017-01-18 00:17:54 +0900

    총알 스크립트 수정

--- Assets/02. Scripts/Bullet.cs
+++ Assets/02. Scripts/Bullet.cs
@@ -3,12 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Bullet : MonoBehaviour {
+	public float time;
+	public float force;
 
-	void Start() {
+	private float dirFactor = 1;
+	private Rigidbody2D rb2d;
+
+	void Awake(){
+		rb2d = GetComponent<Rigidbody2D>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start(){
+		dirFactor = rb2d.velocity.x / Mathf.Abs(rb2d.velocity.x);
+		StartCoroutine(DestroyInTime(time));
+	}
+	void OnCollisionEnter2D(Collision2D coll){
+		if(coll.transform.CompareTag("Player")){
+			Rigidbody2D collRb2d = coll.transform.GetComponent<Rigidbody2D>();
+			collRb2d.AddForce(force * Vector2.right * dirFactor);
+		}
+		Destroy(this.gameObject);
+	}
+
+	IEnumerator DestroyInTime(float sec){
+		yield return new WaitForSeconds(sec);
+
+		Destroy(this.gameObject);
 	}
 }
